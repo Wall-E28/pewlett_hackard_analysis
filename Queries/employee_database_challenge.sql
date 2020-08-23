406cb9a25f13b9e1046a93ed73c360c7648d7ee2
Wall-E28 <rileybrowne@gmail.com>
2020-08-23 14:15:25 -0500

    "Updating data and queries folders"

--- Queries/employee_database_challenge.sql
+++ Queries/employee_database_challenge.sql
@@ -23,3 +23,18 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY titles
 ORDER BY count(emp_no) DESC;
+
+-- Join employee, dept_emp, and title tables to create eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+    e.first_name, e.last_name,e.birth_date,
+    de.from_date, de.to_date,
+    ti.titles
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+    AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
