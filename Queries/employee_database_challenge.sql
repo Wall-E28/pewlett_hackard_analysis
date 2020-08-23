7d3f448cf9b7febaeb31b72223b055cc26a06d18
Wall-E28 <rileybrowne@gmail.com>
2020-08-23 13:44:31 -0500

    "Adding files to Data folder and updating Queries folder"

---
+++ Queries/employee_database_challenge.sql
@@ -0,0 +1,25 @@
+-- Join employee and title tables to create new table "retirement by title"
+SELECT e.emp_no, e.first_name, e.last_name,
+    ti.titles, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti 
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+	
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	titles
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Use unique titles table to create new table "retiring titles"
+SELECT COUNT(emp_no), titles
+INTO retiring_titles
+FROM unique_titles
+GROUP BY titles
+ORDER BY count(emp_no) DESC;
