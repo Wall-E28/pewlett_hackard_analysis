924c3694ba439c8920d9db2cf5508dfed44677cb
Wall-E28 <rileybrowne@gmail.com>
2020-08-23 11:21:24 -0500

    Updating schemas and queries files

--- schemas.sql
+++ schemas.sql
@@ -40,8 +40,7 @@ CREATE TABLE titles (
   titles VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE dept_emp (
