2a9c1c06a0335764cf3ee9aab83c3ec1f97a4428
Wall-E28 <rileybrowne@gmail.com>
2020-08-23 11:54:30 -0500

    Updating queries file

--- queries.sql
+++ queries.sql
@@ -36,3 +36,37 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- View New Table 
 SELECT * FROM retirement_info;
+
+-- Create New Table "retirement_info" including 'emp_no' column
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables to create new table "current_emp"
+SELECT r.emp_no,r.first_name, r.last_name, d.to_date
+INTO current_emp
+FROM retirement_info as r 
+LEFT JOIN dept_emp as d
+ON r.emp_no = d.emp_no
+WHERE d.to_date = ('9999-01-01');
+
+-- Employee count by department number into new table "retirees_by_dept"
+SELECT COUNT(ce.emp_no) AS "Number of Employees", 
+    de.dept_no AS "Department Number"
+INTO retirees_by_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
